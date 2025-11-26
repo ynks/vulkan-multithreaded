51decd153b1585ee809a4d7871853aa0713109dd
Xein <xgonip@gmail.com>
2025-11-27 00:38:40 +0100

    Created vulkan device

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,8 @@ private:
 
 	// device
 	vk::raii::PhysicalDevice m_physicalDevice = nullptr;
+	vk::raii::Device m_vkDevice = nullptr;
+	vk::raii::Queue m_graphicsQueue = nullptr;
 	vk::raii::Instance* m_vk_instance = nullptr;
 
 	void initWindow() {
@@ -43,7 +45,8 @@ private:
 		eNoVertexShader,
 		eNoFragmentShader,
 		eNoComputeShader,
-		eNoPresentationSupport
+		eNoPresentationSupport,
+		eNoQueueFound
 	};
 
 	std::expected<unsigned, DeviceError> getDeviceScore(vk::raii::PhysicalDevice device) {
@@ -110,14 +113,20 @@ private:
 		score += properties.limits.maxViewports;
 		score += properties.limits.maxColorAttachments;
 
+		std::println("Device \"{0}\" scored {1}", static_cast<std::string>(properties.deviceName), score);
+
 		return score;
 	}
 
 	void pickPhysicalDevice() {
+		std::println("Getting physical device...");
+
 		// Get all devices
 		m_vk_instance = m_instance->get();
 		auto devices = m_vk_instance->enumeratePhysicalDevices();
 		if (devices.empty()) throw std::runtime_error("No physical device found");
+		std::println("Found {} devices:", devices.size());
+		for (const auto& d : devices) std::println("\t{}", static_cast<std::string>(d.getProperties().deviceName));
 
 		// Assign score
 		auto bestDevice = std::max_element(devices.begin(), devices.end(),
@@ -137,22 +146,85 @@ private:
 		}
 
 		m_physicalDevice = *bestDevice;
+		std::println("Physical device \"{0}\" chosen", static_cast<std::string>(m_physicalDevice.getProperties().deviceName));
 	}
 
-	uint32_t getQueueFamily(vk::QueueFlagBits type = vk::QueueFlagBits::eGraphics) {
+	std::expected<uint32_t, DeviceError> getQueueFamily(vk::QueueFlagBits type = vk::QueueFlagBits::eGraphics) {
+		if (m_physicalDevice == nullptr) throw std::runtime_error("Trying to get QueueFamily without a device");
+
 		std::vector queue_family_properties = m_physicalDevice.getQueueFamilyProperties();
 		
 		// finds the first queue with the target type
 		auto target = std::find_if(queue_family_properties.begin(), queue_family_properties.end(), [type](const auto& qfp){
 			return qfp.queueFlags & type;
 		});
+		if (target == queue_family_properties.end()) return std::unexpected(DeviceError::eNoQueueFound);
 		return static_cast<uint32_t>(std::distance(queue_family_properties.begin(), target));
+	}
+
+	void createLogicalDevice() {
+		std::println("Creating logical device...");
+
+		// TODO: Search the difference from logical and physical devices
+		// Get queue families
+		std::vector queue_properties = m_physicalDevice.getQueueFamilyProperties();
+		uint32_t graphics_index = getQueueFamily(vk::QueueFlagBits::eGraphics).value();
+		std::println("Got graphics queue at {}", graphics_index);
+
+		// Create queues
+		float graphics_queue_priority = 0.5f;
+		float compute_queue_priority = 0.2f;
+
+		std::vector<vk::DeviceQueueCreateInfo> queues_info = {
+			vk::DeviceQueueCreateInfo {
+				.queueFamilyIndex = graphics_index,
+				.queueCount = 1,
+				.pQueuePriorities = &graphics_queue_priority
+			}
+		};
+
+		// Enable features
+		vk::StructureChain<
+			vk::PhysicalDeviceFeatures2,
+			vk::PhysicalDeviceVulkan13Features,
+			vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT
+		> feature_chain = {
+				{},                               // vk::PhysicalDeviceFeatures2
+				{.dynamicRendering = true },      // Enable dynamic rendering
+				{.extendedDynamicState = true }   // Enable extended dynamic state
+		};
+
+		// Enable extensions
+		std::vector<const char*> device_extensions = {
+			vk::KHRSwapchainExtensionName,
+			vk::KHRSpirv14ExtensionName,
+			vk::KHRSynchronization2ExtensionName,
+			vk::KHRCreateRenderpass2ExtensionName
+		};
+		std::println("Enabling {} device extensions:", device_extensions.size());
+		for (const auto& ext : device_extensions) std::println("\t{}", ext);
+
+		// Create device
+		vk::DeviceCreateInfo device_create_info{
+			.pNext = &feature_chain.get<vk::PhysicalDeviceFeatures2>(),
+			.queueCreateInfoCount = static_cast<uint32_t>(queues_info.size()),
+			.pQueueCreateInfos = queues_info.data(),
+			.enabledExtensionCount = static_cast<uint32_t>(device_extensions.size()),
+			.ppEnabledExtensionNames = device_extensions.data()
+		};
+		m_vkDevice = vk::raii::Device(m_physicalDevice, device_create_info);
+
+		// Retrieve queue
+		m_graphicsQueue = vk::raii::Queue(m_vkDevice, graphics_index, 0);
+
+		std::println("Created Vulkan Device");
 	}
 
 	void initVulkan() {
 		m_instance = std::make_unique<vulkan::Instance>();
 
 		pickPhysicalDevice();
+		createLogicalDevice();
 	}
 
 	void mainLoop() {
